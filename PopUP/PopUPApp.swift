bd4826706a27a2bbf49b2adee71a169d0fd49fcb
Jakubpatelski <jakubpatelski24@gmail.com>
2023-02-26 19:54:06 +0100

    Initial Commit

---
+++ PopUP/PopUPApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopUPApp.swift
+//  PopUP
+//
+//  Created by Jakub Patelski on 26/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct PopUPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
